ba3d59f624a3095b77b003bd946695628797f106
Samuel Davidson <samdamana@gmail.com>
2015-05-04 03:39:31 -0600

    Initial Commit. Added DependenciesTracker and UnitTest.

---
+++ PackageInstaller/DependenciesTester/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PackageInstaller;
+
+namespace DependenciesTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] dependencies =
+            {
+                "KittenService:", "Leetmeme: Cyberportal", "Cyberportal: Ice",
+                "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: "
+            };
+            var DT = new DependenciesTracker(dependencies);
+            Assert.AreEqual(DT.PrintDependencies(),"KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream");
+        }
+    }
+}
---
+++ PackageInstaller/PackageInstaller/DependenciesTracker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageInstaller
+{
+    public class DependenciesTracker
+    {
+        public DependenciesTracker(String[] dependencies)
+        {
+            
+        }
+
+        public String PrintDependencies()
+        {
+            return "";
+        }
+    }
+}
